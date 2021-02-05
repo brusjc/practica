dcb27cdea22aea0b67de7e060d9c48d5e67199a5
brusjc <brusjc02@gmail.com>
2021-02-05 22:01:32 +0100

    arreglo examenes-jqcv vuelta al 1

--- resources/views/paginas/examenes/index.blade.php
+++ resources/views/paginas/examenes/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.frontandia.general')
 
+@section('contentheader_title'){{trans('examenes.jqcv_title')}}@endsection
+
+@section('contentheader_h1'){{ trans('examenes.jqcv_h1') }}@endsection
+
+@section('breadcrumb0')<a href="/{{session('lang')}}/">Inicio</a>@endsection
+
+@section('breadcrumb1'){{session('BC1texto')}}@endsection
+
+@section('descripcion'){{ trans('examenes.jqcv_descripcion') }}@endsection
+
+@section('keywords'){{ trans('examenes.jqcv_keywords') }}@endsection
+
 @section('main_content')
 
 <section class="container justify">
