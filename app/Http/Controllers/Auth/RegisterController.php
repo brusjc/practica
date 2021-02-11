d8997165ebba371236bed892c3f921b36874d45f
brusjc <brusjc02@gmail.com>
2021-02-11 20:59:39 +0100

    arreglo register 13

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,30 +59,6 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $data['confirmation_code'] = Str::random(25);
-        $now = new \DateTime();
-        $data['fechaprivacidad'] = $now->format('d-m-Y H:i:s');
-        $user = User::create([
-            'name'              => $data['name'],
-            'apellido1'         => $data['apellido1'],
-            'apellido2'         => $data['apellido2'],
-            'email'             => $data['email'],
-            'roll_id'           => '1',
-            'password'          => Hash::make($data['password']),
-            'confirmation_code' => $data['confirmation_code'],
-        ]);
-
-        // Enviamos el email de confirmación
-        Mail::send('paginas.emails.confirmation_code', $data, function($message) use ($data) {
-            $message->to($data['email'], $data['name'])->subject('Por favor confirma tu correo');
-        });
-
-        return $user;
-    }
-
-
-    protected function register(array $data)
-    {
-        $data['confirmation_code'] = Str::random(25);
         $data['fechaprivacidad'] = date("d-m-Y H:i:s");
 
         $user = User::create([
