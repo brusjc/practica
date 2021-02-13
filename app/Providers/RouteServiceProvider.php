fe4bd611ecf522a40da4adfe80211975183e9ec2
brusjc <brusjc02@gmail.com>
2021-02-13 19:16:08 +0100

    Update RouteServiceProvider.php
    cambio la ruta home a /va/home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/'.session('lang').'/home';
+    public const HOME = '/va/home';
 
     /**
      * Define your route model bindings, pattern filters, etc.
