9a893ff77a5821597e0cf1bd6671cd3f1fceee7d
brusjc <brusjc02@gmail.com>
2021-02-18 12:51:00 +0100

    arreglo migración user por email 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('apellido1');
             $table->string('apellido2');
             $table->string('email', 250)->unique();
-            $table->timestamp('email_verified_at', 250)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
