38d4be66c73945d18574e29cf99575c0490a1a8e
brusjc <brusjc02@gmail.com>
2021-02-18 12:39:24 +0100

    arreglo migracion user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido1');
+            $table->string('apellido2');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
