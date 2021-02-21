889cdd632587b513fb10ee889e5141e37cda0f4c
brusjc <brusjc02@gmail.com>
2021-02-21 19:04:43 +0100

    arreglo welcome

---
+++ resources/views/welcome-fontandia.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.frontandia.general')
+
+@section('main_content')
+        Hola2
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 //Cambio de idioma
 Route::get('lang/{lang}', function($lang) {
-  \Session::put('lang', $lang);
-  return \Redirect::back();
-})->middleware('web')->name('change_lang');
+      \Session::put('lang', $lang);
+      return \Redirect::back();
+    })->middleware('web')->name('change_lang');
 
 
 Route::get('/', function () { return view('welcome'); });
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+        return view('dashboard');
+    })->name('dashboard');
