a783e7690cca2a4e4a29c75bb779ee5351d14324
brusjc <brusjc02@gmail.com>
2021-02-23 13:51:38 +0100

    Cambio lang en config/app

---
+++ resources/lang/va/authpra.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    //Página de registro
+    'Name' => 'nombre',
+    'E-Mail Address' => 'correo electrónico',
+    'Password' => 'contraseña',
+    'Confirm Password' => 'confirmación contraseña',
+    'register' => 'registro',
+
+    
+    //Páginas de verificación de email
+    'Verify_texto1' => 'Gracias por registrarte! Antes de comenzar, ¿podría verificar su dirección de correo electrónico haciendo clic en el enlace que le acabamos de enviar? Si no recibió el correo electrónico, con gusto le enviaremos otro.',
+    'Verify_texto2' => 'Se ha enviado un nuevo enlace de verificación a la dirección de correo electrónico que proporcionó durante el registro.',
+    'Verify_texto3' => 'Reenviar Email de verificación ',
+    'Verify_texto4' => 'Cerrar sesión',
+    'Verify_texto5' => '',
+    'Verify_texto6' => '',
+    'Verify_texto7' => '',
+    'Verify_texto8' => '',
+    'Verify_texto9' => '',
+    'Verify_texto10' => '',
+
+
+
+
+    'A fresh verification link has been sent to your email address.' => 'Se ha enviado un nuevo enlace de verificación a su dirección de correo electrónico.',
+    'Before proceeding, please check your email for a verification link.' => 'Antes de continuar, revisa tu correo electrónico para obtener un enlace de verificación.',
+    'If you did not receive the email' => 'Si no recibiste el correo electrónico',
+    'click here to request another' => 'haz clic aquí para solicitar otro',
+    
+    'Remember Me' => 'Recuérdame',
+    'Forgot Your Password?' => '¿Olvidaste tu contraseña?',
+    'Register a new membership' => 'Registrar nuevo usuario',
+    
+        
+    //confirmation_code.blade
+    'Confirmación de correo electrónico' =>'Confirmación de correo electrónico',
+    'Hola' => 'Hola',
+    'gracias por registrarte en www.practicavalenciano.es' => 'gracias por registrarte en www.practicavalenciano.es',
+    'Por favor confirma tu correo electrónico' => 'Por favor confirma tu correo electrónico',
+    'Para ello simplemente debes hacer click en el siguiente enlace' => 'Para ello simplemente debes hacer click en el siguiente enlace',
+    'Clic para confirmar tu email' => 'Clic para confirmar tu email',
+
+
+    //reset.blade.php
+    'Reset Password' => 'Restablecer la contraseña',
+    'Send Password Reset Link' => 'Enviar enlace de restablecimiento de contraseña',
+
+
+    //confirm.blade.php
+    'Forgot Your Password?' => '¿Olvidaste tu contraseña?',
+    'Please confirm your password before continuing' => 'Por favor, confirma tu contraseña antes de continuar',
+
+];
